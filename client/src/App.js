b8ae484f4536d686abf8d64267c66c44cc5ba121
ChengEx <frf2isgood@gmail.com>
2021-10-26 20:49:59 +0800

    10/26-1

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { Container } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
@@ -13,12 +13,17 @@ import Register from './pages/Register';
 
 function App() {
   return (
+    
     <Router>
-      <MenuBar/>
-      <Route exact path='/' component={Home}></Route>
-      <Route exact path='/login' component={Login}></Route>
-      <Route exact path='/register' component={Register}></Route>
+      <Container>
+        <MenuBar/>
+        <Route exact path='/' component={Home}></Route>
+        <Route exact path='/login' component={Login}></Route>
+        <Route exact path='/register' component={Register}></Route>
+      </Container>
     </Router>
+    
+    
   );
 }
 
